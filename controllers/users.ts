2f45e9655e6901a48e6fab9d27bb814a68ea0d7f
Nando <nandogoulart.m@gmail.com>
2024-03-30 14:35:32 -0300

    feat(user): user create added

--- controllers/users.ts
+++ controllers/users.ts
@@ -1,7 +1,25 @@
 import { Request, Response } from "express";
+import { IUsers } from "../models/users";
+import UsersRepository from "../models/usersModel";
 
 async function create(req: Request, res: Response) {
-  res.json({ message: "User criado!" });
+  try {
+    const userData = req.body as IUsers;
+    const existingUser = await UsersRepository.findOne({
+      where: { email: userData.email },
+    });
+    if (existingUser) {
+      return res.status(400).json({ message: "Este email já está em uso." });
+    }
+    const newUser = await UsersRepository.create({
+      name: userData.name,
+      email: userData.name,
+      password: userData.name,
+    });
+    res.json({ message: "Usuário criado com sucesso.", user: newUser });
+  } catch (error) {
+    res.json(error);
+  }
 }
 
 export default { create };
