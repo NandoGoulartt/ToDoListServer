c3f7ac69541433ad8bc0b3add06f23dbc7c13778
Nando <nandogoulart.m@gmail.com>
2024-03-31 16:47:37 -0300

    feat(task): put added

--- controllers/tasks.ts
+++ controllers/tasks.ts
@@ -1,4 +1,4 @@
-import { Response } from "express";
+import { Request, Response } from "express";
 import TasksRepository from "../models/tasksModel";
 import { AuthRequest } from "../middleware/auth";
 
@@ -30,23 +30,75 @@ async function getTasks(req: AuthRequest, res: Response) {
   try {
     const userId = req.userId;
     let tasks;
-    if (req.userRoles) {
-      if (!req.userRoles.includes("ADMIN")) {
-        tasks = await TasksRepository.findAll({
-          where: { user_id: userId },
-        });
-      } else {
-        tasks = await TasksRepository.findAll();
-      }
-      return res.json(tasks);
+    if (!req.userRoles!.includes("ADMIN")) {
+      tasks = await TasksRepository.findAll({
+        where: { user_id: userId },
+      });
+    } else {
+      tasks = await TasksRepository.findAll();
     }
+    return res.json(tasks);
   } catch (error) {
     console.error("Erro ao obter todas as tarefas:", error);
     res.status(500).json({ error: "Erro ao obter todas as tarefas." });
   }
 }
 
+async function getTask(req: AuthRequest, res: Response) {
+  try {
+    const { id } = req.params;
+    const task = await TasksRepository.findByPk(id);
+
+    if (!task) {
+      return res.status(404).json({ error: "Tarefa não encontrada." });
+    }
+
+    if (
+      task.dataValues.user_id != req.userId &&
+      !req.userRoles!.includes("ADMIN")
+    ) {
+      return res
+        .status(401)
+        .json({ error: "Você não tem permissão para acessar esta tarefa." });
+    }
+
+    res.json(task);
+  } catch (error) {
+    console.error("Erro ao obter tarefa por ID:", error);
+    res.status(500).json({ error: "Erro ao obter tarefa por ID." });
+  }
+}
+
+async function updateTask(req: AuthRequest, res: Response) {
+  try {
+    const { id } = req.params;
+    const { title, priority } = req.body;
+
+    const task = await TasksRepository.findByPk(id);
+    if (!task) {
+      return res.status(404).json({ error: "Tarefa não encontrada." });
+    }
+
+    if (
+      task.dataValues.user_id != req.userId &&
+      !req.userRoles!.includes("ADMIN")
+    ) {
+      return res
+        .status(401)
+        .json({ error: "Você não tem permissão para alterar esta tarefa." });
+    }
+
+    await task.update({ title, priority });
+    res.json(task);
+  } catch (error) {
+    console.error("Erro ao atualizar tarefa:", error);
+    res.status(500).json({ error: "Erro ao atualizar tarefa." });
+  }
+}
+
 export default {
   createTask,
   getTasks,
+  getTask,
+  updateTask,
 };
--- routes/tasks.ts
+++ routes/tasks.ts
@@ -5,6 +5,7 @@ import { authMiddleware } from "../middleware/auth";
 const router = Router();
 
 router.get("/tasks", authMiddleware, tasksController.getTasks);
+router.get("/tasks/:id", authMiddleware, tasksController.getTask);
 router.post("/tasks/create", authMiddleware, tasksController.createTask);
-
+router.put("/tasks/:id", authMiddleware, tasksController.updateTask);
 export default router;
